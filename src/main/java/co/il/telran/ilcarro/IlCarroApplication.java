158d2f842603d4680d16c3e6c5f29886445c4da0
Maksim Vlasov <vlasovmb@iMac-Maksim.local>
2020-05-02 15:41:44 +0300

    Start empty project Ilcarro without SpringBootSecurityAutoConfiguration()

---
+++ src/main/java/co/il/telran/ilcarro/IlCarroApplication.java
@@ -0,0 +1,14 @@
+package co.il.telran.ilcarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class IlCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlCarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/il/telran/ilcarro/IlCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package co.il.telran.ilcarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
