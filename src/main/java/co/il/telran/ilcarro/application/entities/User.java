cc77b8dbccd0d927f618e6e972f246d1e4a56b26
xbombombomx <54177203+xbombombomx@users.noreply.github.com>
2020-05-04 19:45:23 +0300

    some additions

--- src/main/java/co/il/telran/ilcarro/application/entities/User.java
+++ src/main/java/co/il/telran/ilcarro/application/entities/User.java
@@ -2,13 +2,14 @@ package co.il.telran.ilcarro.application.entities;
 
 
 import co.il.telran.ilcarro.application.security.entities.Account;
-import co.il.telran.ilcarro.application.security.entities.Role;
 import lombok.*;
 
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -25,15 +26,19 @@ public class User extends BaseEntity{
 
     private String firstName;
     private String lastName;
+
+    @Email
+    @Column(unique = true)
     private String email;
+
     private Date birthday;
     private String about;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    private List<Comment> comments = new ArrayList<>();
+    private Set<Comment> comments = new HashSet<>();
 
-    private List<Car> myCars;
-    private List<Car> rentCars;
+    private Set<Car> myCars;
+    private Set<Car> rentCars;
 
 
 }
