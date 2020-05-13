338837e52b22782f57d023d1e77412df67ecdfee
Olha <59340257+OlgaPK@users.noreply.github.com>
2020-05-13 20:46:15 +0300

    Update AuthPair.java
    Validation added. Login can only as an email. Password must have A-Z, a-z , 0-9, and special symbols

--- src/main/java/co/il/telran/ilcarro/application/security/dto/AuthPair.java
+++ src/main/java/co/il/telran/ilcarro/application/security/dto/AuthPair.java
@@ -5,7 +5,13 @@ import lombok.Getter;
 @Getter
 public class AuthPair {
 	
+	@Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+	        +"[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
+	        +"(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+	             message="{invalid email}")
 	private String login;
+	
+	@Pattern(regexp ="^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).*$", message="{invalid.password}")
 	private String password;
 
 }
