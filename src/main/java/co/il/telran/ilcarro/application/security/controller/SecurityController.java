edbc53d20242dfe613eff57deac947a9a033db81
Olha <59340257+OlgaPK@users.noreply.github.com>
2020-05-13 20:49:03 +0300

    Update SecurityController.java
    Validation, Exception handlers added

--- src/main/java/co/il/telran/ilcarro/application/security/controller/SecurityController.java
+++ src/main/java/co/il/telran/ilcarro/application/security/controller/SecurityController.java
@@ -1,15 +1,25 @@
 package application.security.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.security.dto.AccountDto;
@@ -19,12 +29,13 @@ import application.security.services.ISecurityService;
 @RestController
 @RequestMapping("/security")
 @CrossOrigin
+@Validated
 public class SecurityController {
 	
 	@Autowired ISecurityService service;
 	
 	@PostMapping("/addUser") //create new account
-	public AccountDto addUser(@RequestBody AuthPair authPair) {
+	public AccountDto addUser(@RequestBody @Valid AuthPair authPair) {
 		return service.addUser(authPair.getLogin(), authPair.getPassword());
 	};
 
@@ -32,8 +43,7 @@ public class SecurityController {
 	public AccountDto addAccount(@RequestBody AuthPair authPair, @RequestParam String role) {
 		return service.addAccount(authPair.getLogin(), authPair.getPassword(), role);
 	};
-
-		
+	
 	@GetMapping("/getAllAccounts")
 	public List<AccountDto> getAllAccounts(){
 		return service.getAllAccounts();
@@ -44,5 +54,22 @@ public class SecurityController {
 		return service.removeAccount(login);
 	};
 
-	
+	@ExceptionHandler(value = { MethodArgumentNotValidException.class })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<String> handleBodyException(MethodArgumentNotValidException exception) {
+        System.out.println(exception.getBindingResult().getAllErrors());
+        return exception.getBindingResult().getAllErrors().stream()
+                            .map((e)-> e.getObjectName()+": "+e.getDefaultMessage())
+                            .collect(Collectors.toList());
+         
+    }
+     
+    @ExceptionHandler(value = { ConstraintViolationException.class })
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public List<String> handleParamException(ConstraintViolationException exception) {
+        System.out.println("second");
+        return exception.getConstraintViolations().stream()
+                .map((e)-> e.getMessage()+": "+e.getInvalidValue())
+                .collect(Collectors.toList());   
+    }
 }
